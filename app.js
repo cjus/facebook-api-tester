c5e0ea1c74da10bb2f51d5527a88f19be70c1d31
Carlos Justiniano <carlos.justiniano@gmail.com>
2012-10-25 18:31:50 -0400

    a bit of restructuring and safer console logging

--- app.js
+++ app.js
@@ -1,54 +1,4 @@
-var appobj = {};
-(function(app) {
-    var appName = 'Facebook API Tester';
-    var version = '0.1';
-    
-    app.init = function() {
-        console.log(appName + ' ' + version);
-    };
-
-    app.login = function() {
-        FB.login(function(response) {
-            if (response.authResponse) {
-                console.log('Welcome!  Fetching your information.... ');
-                FB.api('/me', function(response) {
-                    console.log('Good to see you, ' + response.name + '.');
-                });
-                app.testAPIs();
-            }
-            else {
-                console.log('User cancelled login or did not fully authorize.');
-            }
-        }, {
-            scope: 'email, user_likes, friends_likes, publish_actions, publish_stream, user_games_activity, friends_games_activity'
-        });
-    };
-
-    app.testAPIs = function() {
-        //appobj.sendInvite("Join me in seeing this work!", "1200172369, 100002891350086, 107015399372765, 714633001");
-        appobj.sendInvite("Join me in seeing this work!", "1200172369");
-    }
-
-    app.sendInvite = function(message, fb_friends) {
-        var obj = {
-            method: 'apprequests',
-            message: message,
-            to: fb_friends
-        };
-
-        FB.ui(obj, function(response) {
-            if (!response || response.error) {
-                console.log('error: ' + response);
-            }
-            else {
-                console.log(response);
-            }
-        });
-    };
-
-})(appobj);
-
-// Load the SDK Asynchronously
+// Load the FB SDK Asynchronously
 (function(d) {
     var js, id = 'facebook-jssdk';
     if (d.getElementById(id)) {
@@ -60,6 +10,63 @@ var appobj = {};
     js.src = "https://connect.facebook.net/en_US/all.js";
     d.getElementsByTagName('head')[0].appendChild(js);
 }(document));
+
+var appobj = {};
+(function(app) {
+    var appName = 'Facebook API Tester';
+    var version = '0.1';
+    
+    app.log = function(obj) {
+        if (window.console && window.console.log) {
+            window.console.log(obj);
+        }
+    };
+
+    app.init = function() {
+        app.log(appName + ' ' + version);
+    };
+
+    app.login = function() {
+        FB.login(function(response) {
+            if (response.authResponse) {
+                app.log('Welcome!  Fetching your information.... ');
+                FB.api('/me', function(response) {
+                    app.log('Good to see you, ' + response.name + '.');
+                });
+                app.testAPIs();
+            }
+            else {
+                app.log('User cancelled login or did not fully authorize.');
+            }
+        }, {
+            //scope: 'email, user_likes, friends_likes, publish_actions, publish_stream, user_games_activity, friends_games_activity'
+        });
+    };
+
+    app.testAPIs = function() {
+        //appobj.sendInvite("Join me in seeing this work!", "1200172369, 100002891350086, 107015399372765, 714633001");
+        appobj.sendInvite("Join me in seeing this work!", "1200172369");
+    };
+
+    app.sendInvite = function(message, fb_friends) {
+        var obj = {
+            method: 'apprequests',
+            message: message,
+            to: fb_friends
+        };
+
+        FB.ui(obj, function(response) {            
+            if (!response) {
+                app.log('Unknown error');
+            } else if (response.error) {
+                app.log('error: ' + response.error);            
+            } else {
+                app.log(response);
+            }
+        });
+    };
+
+})(appobj);
 
 $(document).ready(function() {
     window.fbAsyncInit = function() {
